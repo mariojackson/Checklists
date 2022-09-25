ee578ea072a653f52b8c7a1d84c303d05c1192a3
Mario Jackson <mario@jackson.softwre>
2022-09-25 18:25:27 +0200

    Initial Commit

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ChecklistViewController.swift
+//  Checklists
+//
+//  Created by Mario Jackson on 9/24/22.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+}
+
