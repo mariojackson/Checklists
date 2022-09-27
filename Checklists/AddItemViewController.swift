7b28f31a15f7ce54f3ff9c56befdbfb1e175e855
Mario Jackson <mario@jackson.softwre>
2022-09-27 21:00:43 +0200

    AddItem table table row should be an input

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,5 +23,18 @@ class AddItemViewController: UITableViewController {
     
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 }
