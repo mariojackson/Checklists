ae82532569087528296d7f80ca95719f051137de
Mario Jackson <mario@jackson.softwre>
2022-11-10 17:07:58 +0100

    Setup ListDetailViewController

---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Mario Jackson on 11/10/22.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: AnyObject {
+    func listDetailViewControllerDidCancel(
+        _ controller: ListDetailViewController
+    )
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishAdding checklist: Checklist
+    )
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishEditing checklist: Checklist
+    )
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let existingChecklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = existingChecklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - Actions
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let existingChecklist = checklistToEdit {
+            existingChecklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditing: existingChecklist)
+            return
+        }
+        
+        let checklist = Checklist(name: textField.text!)
+        delegate?.listDetailViewController(self, didFinishAdding: checklist)
+    }
+    
+    // MARK: - Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let originalText = textField.text!
+        let stringRange = Range(range, in: originalText)!
+        
+        let text = originalText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !text.isEmpty
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+}
