315dc019cf181858a2c0646a0fd5afe0ab36f816
Mario Jackson <mario@jackson.software>
2022-11-29 21:55:58 +0100

    Change default checklist icon to be transparent

--- Checklists/Models/Checklist.swift
+++ Checklists/Models/Checklist.swift
@@ -10,7 +10,7 @@ import UIKit
 class Checklist: NSObject, Codable {
     var name: String
     var items = [ChecklistItem]()
-    var iconName = "Appointments"
+    var iconName = "No Icon"
     
     init(name: String) {
         self.name = name
